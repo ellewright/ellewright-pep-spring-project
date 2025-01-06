4bc8cdd528c3d8f8c0e23a7b8fa6401d431e2828
Ellie Wright <elliewright055@gmail.com>
2025-01-06 21:27:25 +0000

    Begin work on project

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,23 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.repository.MessageRepository;
+import com.example.entity.Message;
+
+import java.util.List;
+
+@Service
 public class MessageService {
+    private MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public List<Message> getAllMessages() {
+        return (List<Message>) messageRepository.findAll();
+    }
 }
